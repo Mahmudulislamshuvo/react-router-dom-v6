38791d14a63f6d9e85a62750d16a4b7c7ecd5705
Mahmudul Islam <122343396+Mahmudulislamshuvo@users.noreply.github.com>
2025-12-20 00:02:07 +0600

    Actual what we will going to use

--- src/main.jsx
+++ src/main.jsx
@@ -1,58 +1,68 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+// Component Imports
 import Root from "./Root";
 import ErrorPage from "./ErrorPage";
 import Contact from "./Contact";
+import EditContact from "./EditContact";
+import Index from "./Index";
+
+// Loader Imports
 import { getContactLoader, getContactsLoader } from "./loaders/contactsLoader";
+
+// Action Imports
 import {
   createContactAction,
   deleteAction,
   editContactAction,
   FevoriteAction,
 } from "./actions/contactsAction";
-import EditContact from "./EditContact";
-import Index from "./Index";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    loader: getContactsLoader,
-    action: createContactAction,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        errorElement: <ErrorPage />,
-        children: [
-          {
-            index: true,
-            element: <Index />,
-          },
-          {
-            path: "/contacts/:contactId",
-            element: <Contact />,
-            loader: getContactLoader,
-            action: FevoriteAction,
-          },
-          {
-            path: "/contacts/:contactId/edit",
-            element: <EditContact />,
-            loader: getContactLoader,
-            action: editContactAction,
-          },
-          {
-            path: "contacts/:contactId/destroy",
-            action: deleteAction,
-            // This is work for that path only
-            errorElement: <p>Opps! There was an error Deleting Contact</p>,
-          },
-        ],
-      },
-    ],
-  },
-]);
+// Router Configuration using JSX Style
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path="/"
+      element={<Root />}
+      loader={getContactsLoader}
+      action={createContactAction}
+      errorElement={<ErrorPage />}
+    >
+      {/* Pathless Route wrapper for handling child errors inside the Outlet */}
+      <Route errorElement={<ErrorPage />}>
+        <Route index element={<Index />} />
+
+        <Route
+          path="contacts/:contactId"
+          element={<Contact />}
+          loader={getContactLoader}
+          action={FevoriteAction}
+        />
+
+        <Route
+          path="contacts/:contactId/edit"
+          element={<EditContact />}
+          loader={getContactLoader}
+          action={editContactAction}
+        />
+
+        <Route
+          path="contacts/:contactId/destroy"
+          action={deleteAction}
+          errorElement={<p>Opps! There was an error Deleting Contact</p>}
+        />
+      </Route>
+    </Route>
+  )
+);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
